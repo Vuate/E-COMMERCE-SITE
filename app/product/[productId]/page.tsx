d76cafb1c8c0877f16b1a4e439787960eba780fe
Vuate <ardaemreci2@gmail.com>
2025-06-27 18:49:58 +0300

    Fix dynamic page props type for Vercel build

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -2,23 +2,18 @@ import DetailClient from '@/app/components/detail/DetailClient';
 import React from 'react';
 import { products } from '@/utils/Products';
 
-type Props = {
-  params: {
-    productId: string;
-  };
-};
+interface PageProps {
+  params: { productId: string };
+}
 
-
-const Detail = async ({ params }: Props) => {
+export default async function Page({ params }: PageProps) {
   const { productId } = params;
 
-  const product = products.find(product => product.id == productId);
+  const product = products.find((product) => product.id == productId);
 
   return (
     <div>
       <DetailClient product={product} />
     </div>
   );
-};
-
-export default Detail;
+}
